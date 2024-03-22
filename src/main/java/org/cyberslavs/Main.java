46b3b1a3265f12b170c7b0ae846b0b8b5d768214
public_hysteria <juuzodes@yandex.ru>
2024-03-22 14:42:05 +0300

    added jsoup

--- src/main/java/org/cyberslavs/Main.java
+++ src/main/java/org/cyberslavs/Main.java
@@ -1,7 +1,20 @@
 package org.cyberslavs;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://rostender.info/region/sankt-peterburg-gorod")
+                .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                .get();
+        Elements tenders = doc.select(".tender-info__description");
+        for (Element element : tenders.select("a"))
+            System.out.println(element.text());
+
     }
 }
