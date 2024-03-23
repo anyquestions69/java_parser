2161cac53187e8e7df2dac2d1b5689a5cfb39905
public_hysteria <juuzodes@yandex.ru>
2024-03-23 14:40:08 +0300

    added sping boot

---
+++ src/main/java/org/cyberslavs/parser/ParserApplication.java
@@ -0,0 +1,13 @@
+package org.cyberslavs.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParserApplication.class, args);
+	}
+
+}
